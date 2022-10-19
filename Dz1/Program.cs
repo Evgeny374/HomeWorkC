c5e2177fc0ad1466153e1741065a36555b3ea76f
E.Barabanov <barabanov698@gmail.com>
2022-10-19 14:29:08 +0300

    Задача 2

--- Dz1/Program.cs
+++ Dz1/Program.cs
@@ -1,25 +1,54 @@
-﻿/* Напишите программу, которая на вход принимает два числа и выдаёт,
+﻿/* Задача 2. Напишите программу, которая на вход принимает два числа и выдаёт,
 какое число большее, а какое меньшее */
 
 Console.Clear();
 
-Console.Write("Введите первое число: ");
-int number1 = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите второе  число: ");
-int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе  число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 != number2)
-if (number1 > number2)
-{
-  Console.WriteLine("Первое число больше");  
-  Console.WriteLine("Второе число меньше");
-}
-else
-{
-  Console.WriteLine("Первое число меньше");  
-  Console.WriteLine("Второе число больше");  
-}
-else
-{
-  Console.WriteLine("Два числа равны");
-}
+// if (number1 != number2)
+// if (number1 > number2)
+// {
+//   Console.WriteLine("Первое число больше");  
+//   Console.WriteLine("Второе число меньше");
+// }
+// else
+// {
+//   Console.WriteLine("Первое число меньше");  
+//   Console.WriteLine("Второе число больше");  
+// }
+// else
+// {
+//   Console.WriteLine("Два числа равны");
+// }
+
+/* Задача 4. Напишите программу, которая принимает на вход три числа и выдаёт максимальное
+из этих чисел. */
+
+// Console.Write("Введите первое число: ");
+// int number1 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите второе число: ");
+// int number2 = Convert.ToInt32(Console.ReadLine());
+// Console.Write("Введите третье число: ");
+// int number3 = Convert.ToInt32(Console.ReadLine());
+
+// int max = 0;
+// if (number1 > number2)
+// {
+//     max = number1;
+// }
+// else
+// {
+//     max = number2; 
+// }
+// if (max > number3)
+// {
+//     max = max;
+// }
+// else
+// {
+//     max = number3; 
+// }
+// Console.WriteLine("Максимальное число: " + max);
