07b24249c5b2c499f6856803d7f195c983d79d1a
E.Barabanov <barabanov698@gmail.com>
2022-10-24 15:55:08 +0300

    Задача 6

--- Dz2/Program.cs
+++ Dz2/Program.cs
@@ -1,13 +1,28 @@
 ﻿/* Задача 10. Напишите программу, которая принимает на вход 
 трехзначное число и на выходе показывает вторую цифру этого числа.*/
 
+// Console.Write ("Введите трехзначное число: ");
+// var number  = Console.ReadLine();
+// if (number.Length == 3) 
+// {
+//     Console.WriteLine(number[1]);
+// }
+// else 
+// {
+//     Console.Write ("Введенное число не трехзначное: ");
+// }
+
+
+/*  Напишите программу, которая выводит третью цифру заданного числа или сообщает, что 
+третьей цифры нет.*/
+
 Console.Write ("Введите трехзначное число: ");
-var number  = Console.ReadLine();
-if (number.Length == 3) 
+string number  = Console.ReadLine();
+if (number.Length >= 3) 
 {
-    Console.WriteLine(number[1]);
+    Console.WriteLine(number[2]);
 }
 else 
 {
-    Console.Write ("Введенное число не трехзначное: ");
+    Console.Write ("Третьей цифры в числе нет");
 }
